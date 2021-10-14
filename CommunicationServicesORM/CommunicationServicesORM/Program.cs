4bf4743cc721ee43f4717a90975e55a0a9e5b91a
AlexandraLis295 <sasha-lisik@yandex.ru>
2021-10-15 00:14:12 +0300

    1. Создали классы, соответствующие сущностям базы данных прошлого семестра.
    2. Создали в классах несколько объектов, соответствующих палям таблиц базы данных из прошлого семестра.
    3. Создали конструкторы к соответствующим классам.
    4. В каждом классе, соответствующем полям, переопределили метод  "ToString".
    5. Создали класс, где предусмотрели главный метод и вывод тестовых значений на консоль.
    6. Первая попытка создать зачатки документации.

---
+++ CommunicationServicesORM/CommunicationServicesORM/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationServicesORM
+{
+    class Program
+    {
+        /// <summary>
+        /// Главный метод
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var operators = new CommunicationServicesORM.Operators(1, "Мегафон");
+            Console.WriteLine(operators);
+
+            var services = new CommunicationServicesORM.Services(1, "Пакет СМС", 50);
+            Console.WriteLine(services);
+
+            var Subscribers1 = new CommunicationServicesORM.Subscribers(1, "Рябова", "Дария", "Кирилловна", 1990, 04, 30);
+            Console.WriteLine(Subscribers1);
+            var Subscribers2 = new CommunicationServicesORM.Subscribers(2, "Уварова", "Вероника", "", 1997, 06, 04);
+            Console.WriteLine(Subscribers2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CommunicationServicesORM/CommunicationServicesORM/Subscribers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationServicesORM
+{
+    class Subscribers
+    {
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="lastName">Фамилия</param>
+        /// <param name="firstName">Имя</param>
+        /// <param name="patronymic">Отчество</param>
+        /// <param name="yearOfBirth">Год рождения</param>
+        /// <param name="monthOfBirth">Месяц рождения</param>
+        /// <param name="dayOfBirth">День рождения</param>
+        public Subscribers(int id, string lastName, string firstName, string patronymic, int yearOfBirth, int monthOfBirth, int dayOfBirth)
+        {
+            ID = id;
+            if (lastName == null)
+                throw new ArgumentNullException(nameof(lastName), "LastName cannot be null");
+            LastName = lastName;
+            if (firstName == null)
+                throw new ArgumentNullException(nameof(firstName), "FirstName cannot be null");
+            FirstName = firstName;
+            Patronymic = patronymic;
+            YearOfBirth = yearOfBirth;
+            MonthOfBirth = monthOfBirth;
+            DayOfBirth = dayOfBirth;
+        }
+        public int ID { get; protected set; }
+        public string LastName { get; protected set; }
+        public string FirstName { get; protected set; }
+        public string Patronymic { get; protected set; }
+        public int YearOfBirth { get; protected set; }
+        public int MonthOfBirth { get; protected set; }
+        public int DayOfBirth { get; protected set; }
+        /// <summary>
+        /// Переопределение метода
+        /// </summary>
+        /// <returns>Возвращает строковое представление значения, представляемого объектом. Учтена возможность отсутствия у абонента отчества</returns>
+        public override string ToString()
+        {
+            if (Patronymic == null)
+                return String.Format("ID: {0}, Фамилия: {1}, Имя: {2}, Дата рождения: {3}.{4}.{5}\n", ID, LastName, FirstName, YearOfBirth, MonthOfBirth, DayOfBirth);
+            return String.Format("ID: {0}, Фамилия: {1}, Имя: {2}, Отчество: {3}, Дата рождения: {4}.{5}.{6}\n", ID, LastName, FirstName, Patronymic, YearOfBirth, MonthOfBirth, DayOfBirth);
+        }
+
+    }
+}
+
